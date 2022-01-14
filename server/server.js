e73fbb60e915fc95eb23679e6c56a25055267dc4
Ross Brown <rossbrown22@hotmail.co.uk>
2022-01-14 12:55:35 +0000

    server.js updated

--- server/server.js
+++ server/server.js
@@ -6,3 +6,16 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+
+MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true})
+.then((client) => {
+    const db = client.db('wonders_db');
+    const wondersCollection = db.collection('wonders');
+    const wondersRouter = createRouter(wondersCollection);
+    app.use('/api/wonders', wondersRouter);
+})
+.catch(console.error)
+
+app.listen(5000, function () {
+    console.log(`Listening on port ${ this.adress().port}`)
+});
