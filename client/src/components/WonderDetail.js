089b131bf606dff6ec539fb0a723a065238a7239
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2022-01-15 12:24:53 +0000

    factsListenedTo array

--- client/src/components/WonderDetail.js
+++ client/src/components/WonderDetail.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Speech from 'react-speech'
 import './WonderDetail.css'
 
 
+
 const WonderDetail = ({wonder}) => {
+    
+    const [factsListenedTo, setFactsListenedTo] = useState([]);
+    
+    const handleClickFact1=() => {
+        const updatedFactsListenedTo = [...factsListenedTo, "fact 1"];
+        setFactsListenedTo(updatedFactsListenedTo);        
+    }
+
+    const handleClickFact2=() => {
+        const updatedFactsListenedTo = [...factsListenedTo, "fact 2"];
+        setFactsListenedTo(updatedFactsListenedTo);
+    }
+
+    const handleClickFact3=() => {
+        const updatedFactsListenedTo = [...factsListenedTo, "fact 3"];
+        setFactsListenedTo(updatedFactsListenedTo);
+    }
+    
+ 
+
     return(
         <div className='wonder-detail'>
             <h2>{wonder.name}</h2>
             <Speech text={wonder.name} textAsButton={true} displayText="▶️" voice="Google UK English Female" />
-            <p>Location: {wonder.location}</p>
-
             <img src={`/img/${wonder.image}`} alt={wonder.name} />
             
             <p>{wonder.facts[0]}</p>
-            <Speech text={wonder.facts[0]} textAsButton={true} displayText="▶️" voice="Google UK English Female" />
+            <div onClick={handleClickFact1}><Speech text={wonder.facts[0]} textAsButton={true} displayText="▶️" voice="Google UK English Female" /></div>
             <p>{wonder.facts[1]}</p>
-            <Speech text={wonder.facts[1]} textAsButton={true} displayText="▶️" voice="Google UK English Female" />
+            <div onClick={handleClickFact2}><Speech text={wonder.facts[1]} textAsButton={true} displayText="▶️" voice="Google UK English Female" /></div>
             <p>{wonder.facts[2]}</p>
-            <Speech text={wonder.facts[2]} textAsButton={true} displayText="▶️" voice="Google UK English Female" />
+            <div onClick={handleClickFact3}><Speech text={wonder.facts[2]} textAsButton={true} displayText="▶️" voice="Google UK English Female" /></div>
 
             <p>Fun Facts: {wonder.funFact}</p>
             
