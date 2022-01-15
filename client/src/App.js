788de258f43109e8b79d5fd65b0b1a5532416556
Ross Brown <rossbrown22@hotmail.co.uk>
2022-01-15 19:22:14 +0000

    added quiz page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import New from "./components/New";
 import Ancient from './components/Ancient';
 import Natural from './components/Natural';
+import Quiz from './components/Quiz';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/new" component={New} />
       <Route exact path="/ancient" component={Ancient} />
       <Route exact path="/natural" component={Natural} />
+      <Route exact path="/quiz" component={Quiz} />
     </>
     </Router>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,6 +16,11 @@ const Home = () => {
             e.preventDefault();
             window.location.href="http://localhost:3000/natural";
             }}>🏔<br />Natural</button>
+
+            <button type="button" onClick={(e) => {
+            e.preventDefault();
+            window.location.href="http://localhost:3000/quiz";
+            }}>📝<br />Quiz</button>
         </>
     )
 }
