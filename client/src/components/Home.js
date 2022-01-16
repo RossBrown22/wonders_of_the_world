4927bcbb14d047f1996abcf5c7de4ad6c11e79f3
Ross Brown <rossbrown22@hotmail.co.uk>
2022-01-16 15:26:12 +0000

    buttons and animations done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,13 +2,17 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className="home-page">
-            <h1>The Wonders of The World</h1>
-            <a href="/new"><button type="button">🕌<br />New</button></a>
-            <a href="/ancient"><button type="button">🏛<br />Ancient</button></a>
-            <a href="/natural"><button type="button">🏔<br />Natural</button></a>
-        <img src={"/img/Rotating_globe.gif"}></img>
-        </div>
+        <>
+            <div className="home-page">
+                <h1>The Wonders of The World</h1>
+            <img src={"/img/Rotating_globe.gif"}></img>
+            </div>
+            <div className='front-btn'>
+                <a href="/new"><button type="button"><span>🕌<br />New</span></button></a>
+                <a href="/ancient"><button type="button"><span>🏛<br />Ancient</span></button></a>
+                <a href="/natural"><button type="button"><span>🏔<br />Natural</span></button></a>
+            </div>
+        </>
     )
 }
 
