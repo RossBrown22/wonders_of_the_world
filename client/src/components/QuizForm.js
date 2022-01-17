8f5a31b5266abc53b6c70eb7e8e254d8b1af9e9f
Ross Brown <rossbrown22@hotmail.co.uk>
2022-01-17 09:47:13 +0000

    started question/answer feature

--- client/src/components/QuizForm.js
+++ client/src/components/QuizForm.js
@@ -1,8 +1,39 @@
 import { useState } from 'react'
 
-const QuizForm = () => {
+const QuizForm = ({questions, onQuizSubmit}) => {
+
+    const [submittedAnswers, setSubmittedAnswers] = useState([])
+
+    const handleAnswerChange = (evt) => {
+        setSubmittedAnswers(evt.target.value)
+    }
+
+    const handleQuizSubmit = (evt) => {
+        evt.preventDefault()
+
+        onQuizSubmit({
+            submittedAnswers: submittedAnswers
+        })
+    }
+
     return (
-        <h1>This is the quiz form!</h1>
+        <div>
+            <h1>This is the quiz form!</h1>
+            <form onSubmit={handleQuizSubmit}>
+                <h3>1. {questions[0].question}</h3>
+
+                <label>{questions[0].choices[0]}</label>
+                <input onChange={handleAnswerChange} name="question1" type="radio" value={questions[0].choices[0]}/>
+
+                <label>{questions[0].choices[1]}</label>
+                <input onChange={handleAnswerChange} name="question1" type="radio" value={questions[0].choices[1]}/>
+
+                <label>{questions[0].choices[2]}</label>
+                <input onChange={handleAnswerChange} name="question1" type="radio" value={questions[0].choices[2]}/>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
 
--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -3,12 +3,21 @@ import QuizForm from '../components/QuizForm'
 
 const QuizContainer = () => {
     const [submittedAnswers, setSubmittedAnswers] = useState([])
+    const questions = [{
+        question: "Which country is Petra in?",
+        choices: ["China", "Brazil", "Jordan"],
+        answer: "Jordan"}
+    ]
+
+    const onQuizSubmit = (submittedAnswers) => {
+        setSubmittedAnswers(submittedAnswers)
+    }
 
     return (
-        <>
-        <h1>This is the quiz container!</h1>
-        <QuizForm />
-        </>
+        <div>
+            <h1>This is the quiz container!</h1>
+            <QuizForm questions={questions} onQuizSubmit={onQuizSubmit}/>
+        </div>
     )
 
 }
