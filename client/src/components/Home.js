947867e92a657ef80a16163ef95f3a24bdeb3276
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2022-01-17 15:26:16 +0000

    pop up form working

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,23 @@
 import UsersContainer from '../containers/UsersContainer';
+import PopUpWindow from './PopUpWindow';
 import './Home.css'
+import { useState } from 'react';
 
 const Home = () => {
+    const [popUpOpen, setPopUpOpen] = useState(false)
+    
+    const togglePopUpWindow = () => {
+        setPopUpOpen(!popUpOpen)
+    }
+
     return (
         <div className="home-page">
-                <h1>The Wonders of The World</h1>
+            <div className="home-page-content">
+            <h1>The Wonders of The World</h1>
             <img src={"/img/Rotating_globe.gif"}></img>
-            <UsersContainer />
+            <button className="sign-in-button" type="button" onClick={togglePopUpWindow}>Sign-in</button> 
+            {popUpOpen && <PopUpWindow content={<UsersContainer />} handlePopUpClose={togglePopUpWindow}/>}
+            </div>
             <div className='front-btn'>
                 <a href="/new"><button type="button"><span>🕌<br />New</span></button></a>
                 <a href="/ancient"><button type="button"><span>🏛<br />Ancient</span></button></a>
