a41cfde6b8e76f756fd3288dda35a794a2b11bd1
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2022-01-18 11:54:44 +0000

    seeds updated with all wonder coords

--- client/src/components/MapBox.js
+++ client/src/components/MapBox.js
@@ -5,7 +5,6 @@ import L from 'leaflet'
 import Icon_new_wonders1 from '../icons/Icon_new_wonders1.png'
 import Icon_natural_wonders1 from '../icons/Icon_natural_wonders1.png'
 import Icon_ancient_wonders1 from '../icons/Icon_ancient_wonders1.png'
-import MarkerIcon from './MarkerIcon'
 
 
 const MapBox = ({onWonderSelected, wonders}) => {
@@ -15,32 +14,10 @@ const MapBox = ({onWonderSelected, wonders}) => {
         onWonderSelected(wonders[index])
     }
 
-    const markers = {
-        coords: [
-            { lat: 41.19197, lng: 25.33719 },
-            { lat: 41.26352, lng: 25.1471 },
-            { lat: 41.26365, lng: 25.24215 },
-            { lat: 41.26369, lng: 25.33719 },
-            { lat: 41.26365, lng: 25.43224 },
-            { lat: 41.26352, lng: 25.52728 },
-            { lat: 41.2633, lng: 25.62233 },
-            { lat: 41.263, lng: 25.71737 },
-            { lat: 41.3082, lng: 22.95892 },
-            { lat: 41.31041, lng: 23.054 }
-        ],
-        zoom: 7
-    };
-
-    // const handleClick=() => {
-    // onWonderSelected(wonder)
-    // }
-
-
     const icon_new = L.icon({
         iconUrl: Icon_new_wonders1,
         iconSize: [35, 35],
     });
-
 
     const icon_ancient = L.icon({
         iconUrl: Icon_ancient_wonders1,
@@ -52,197 +29,38 @@ const MapBox = ({onWonderSelected, wonders}) => {
         iconSize: [35, 35],
     });
 
-
+    
     const markerNodes = 
         wonders.map((wonder, index) => {
+            let marker = null
+            const selectMarkerIcon = () => {
+                if (wonder.icon === "icon_new") {
+                    marker = icon_new
+                } else if (wonder.icon === "icon_ancient") {
+                    marker = icon_ancient
+                } else {
+                    marker = icon_natural
+                }
+            }
+            selectMarkerIcon()
             
-            return(
-                
-            <Marker key={index} position={[wonder.coords.lat, wonder.coords.lng]} icon={icon_new}>
-                <Popup>
-                    {wonder.name} <br /> <Link to={wonder.collection}><button value={index} onClick={handleClick} type="button" >Learn More</button></Link>
-                </Popup>
-            </Marker>
+            return( 
+                <Marker key={index} position={[wonder.coords.lat, wonder.coords.lng]} icon={marker}>
+                    <Popup>
+                        {wonder.name} <br /> <Link to={wonder.collection}><button value={index} onClick={handleClick} type="button" >Learn More</button></Link>
+                    </Popup>
+                </Marker>
             )
         })
-    
-
 
 
     return (
 
-        <MapContainer center={[10.000, 0.00]} zoom={2} scrollWheelZoom={true}
-            minZoom={2} maxZoom={14} maxBoundsViscosity={1} noWrap={true} maxBounds={[-90, -180], [90, 180]}
+        <MapContainer center={[10.000, 0.00]} zoom={2} scrollWheelZoom={true} minZoom={2} maxZoom={14} maxBoundsViscosity={1} noWrap={true} maxBounds={[-90, -180], [90, 180]}>
 
-        >
-
-
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-            />
-
-            {markers.coords.map(({ lat, lng }, index) => (
-                <Marker position={[lat, lng]} key={index}>
-                    <Popup>
-                        {index + 1} is for popup with lat: {lat} and lon {lng}
-                    </Popup>
-                </Marker>
-            ))}
+            <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
             {markerNodes}
-
-
-
-            {/* {wonders.map(wonder => {
-        return(
-
-                <MarkerIcon wonder={wonder} onWonderSelected={onWonderSelected}/>
-            
-        )
-    })} */}
-
-
-
-            {/* NEW  */}
-
-            {/* <Marker position={[30.323337, 35.451666]} icon={icon_new}>
-        <Popup>
-        Petra <br /> <Link to="/new"><button type="button" onClick={handleClick}>Learn More</button></Link>
-        </Popup>
-    </Marker> */}
-
-
-            {/* <Marker position={[40.4319077, 116.5681862]} icon={icon_new}>
-                <Popup>
-                    Great Wall <br /> <Link to="/new"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker> */}
-
-
-            <Marker position={[-22.951916, -43.2126759]} icon={icon_new}>
-                <Popup>
-                    Christ the Redeemer <br /> <Link to="/new"><button type="button">Learn More</button></Link>
-                    {/* <button><a href="/new">Learn More</a></button>. */}
-                </Popup>
-            </Marker>
-
-            <Marker position={[41.8902821, 12.4878589]} icon={icon_new}>
-                <Popup>
-                    Colosseum <br /> <Link to="/new"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[-13.1631412, -72.5471516]} icon={icon_new}>
-                <Popup>
-                    Machu Pichu <br /> <Link to="/new"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[20.6787816, -88.5706656]} icon={icon_new}>
-                <Popup>
-                    Chichen Itza <br /> <Link to="/new"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[27.1751448, 78.0399535]} icon={icon_new}>
-                <Popup>
-                    Taj Mahal <br /> <Link to="/new"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-
-            {/* ANCIENT  */}
-
-            <Marker position={[29.9792345, 31.1320132]} icon={icon_ancient}>
-                <Popup>
-                    Great Pyramid of Giza <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[32.4912413, 44.435228]} icon={icon_ancient}>
-                <Popup>
-                    Hanging Gardens of Babylon <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[37.9493601, 27.3627619]} icon={icon_ancient}>
-                <Popup>
-                    Temple of Artemis <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[37.8022947, 22.1210547]} icon={icon_ancient}>
-                <Popup>
-                    Statue of Zeus <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[37.0378874, 27.4219277]} icon={icon_ancient}>
-                <Popup>
-                    Mausoleum At Halicarnassus  <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[36.4509959, 28.2236147]} icon={icon_ancient}>
-                <Popup>
-                    Colossus of Rhodes <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[31.2124397, 29.887799]} icon={icon_ancient}>
-                <Popup>
-                    Lighthouse of Alexandria <br /> <Link to="/ancient"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-
-            {/* NATURAL */}
-
-            <Marker position={[63.8793171, -22.4517288]} icon={icon_natural}>
-                <Popup>
-                    Aurora Borealis <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[36.0997623, -112.1212394]} icon={icon_natural}>
-                <Popup>
-                    Grand Canyon <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[-19.3401041, 145.40679]} icon={icon_natural}>
-                <Popup>
-                    Great Barrier Reef <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[-22.8197641, -43.2922626]} icon={icon_natural}>
-                <Popup>
-                    Harbor of Rio de Janeiro  <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[27.9881199, 86.9162203]} icon={icon_natural}>
-                <Popup>
-                    Mount Everest <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[19.4933462, -102.2601524]} icon={icon_natural}>
-                <Popup>
-                    Paricutin Volcano <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-            <Marker position={[-17.9270587, 25.8437687]} icon={icon_natural}>
-                <Popup>
-                    Victoria Falls <br /> <Link to="/natural"><button type="button">Learn More</button></Link>
-                </Popup>
-            </Marker>
-
-
 
         </MapContainer>
     )
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -85,118 +85,146 @@ db.wonders.insertMany([
         collection: "/new",
         image: "TajMahal.jpeg",
         coords: {lat: 27.1751448, lng: 78.0399535},
-        icon: "icon_new"}
-    // ,
-    // {
-    //     name: "Great Pyramid of Giza",
-    //     location: "Egypt",
-    //     facts: ["It is more than five thousand years old.", "It's estimated that it took well more than two million stone blocks to make.", "It was built for a pharaoh (King) named Khufu."],
-    //     funFact: "When it was built it was 481 feet tall, however because of erosion, it is now 455 feet tall!",
-    //     collection: "/ancient",
-    //     image: "Pyramid.jpeg"
-    // },
-    // {
-    //     name: "Hanging Gardens of Babylon",
-    //     location: "Iraq",
-    //     facts: ["The gardens were thought to be about 75 feet high.", "They were built when King Nebuchadnezzar The Second was in power in Babylon.", "The plants did not actually hang. They grew from many different levels of terraces (similar to balconies)."],
-    //     funFact: "It would have taken 8,200 gallons of water each day to keep the plants watered. Which is roughly the same as 18,500, 2 litres bottles!",
-    //     collection: "/ancient",
-    //     image: "Gardens.jpeg"
-    // },
-    // {
-    //     name: "Temple of Artemis",
-    //     location: "Turkey",
-    //     facts: ["The temple was burnt down by Herostratus, as he believed it would 'make him famous'.", "It was used as a house of worship to Artemis and as well as a marketplace.", "The remains of the structure were first discovered in 1869 by John Turtle Wood."],
-    //     funFact: "It may have been the first ever building which was constructed of Marble!",
-    //     collection: "/ancient",
-    //     image: "Artemis.jpeg"
-    // },
-    // {
-    //     name: "Statue of Zeus",
-    //     location: "Greece",
-    //     facts: ["It was made by the Greek sculptor Phidias around 435 BC.", "The statue was built with ivory plates and gold panels on a wooden framework.", "Zeus was the sky and thunder god in ancient Greek religion."],
-    //     funFact: "The statue was so tall, that if Zeus was to 'stand up', the roof would fall off the temple!",
-    //     collection: "/ancient",
-    //     image: "Zeus.jpeg"
-    // },
-    // {
-    //     name: "Mausoleum At Halicarnassus",
-    //     location: "Turkey",
-    //     facts: ["Construction was finished around 350 BC.", "At the very top, there was a large statue of Mausolus and Artemisia in a chariot pulled by four horses.", "It was mainly built using white marble."],
-    //     funFact: "Almost 19 centuries after it was completed, the Mausoleum was destroyed by earthquakes in the 1300s!",
-    //     collection: "/ancient",
-    //     image: "Mausoleum.jpeg"
-    // },
-    // {
-    //     name: "Colossus of Rhodes",
-    //     location: "Island of Rhodes",
-    //     facts: ["It was a symbol of strength and freedom for the people of Rhodes.", "There are rumours that a new Colossus may be built at Rhodes Harbour at some point in the future.", "Historians believe that 900 camels were used to carry away all the scrap metal."],
-    //     funFact: "It was 108 feet tall, which is nearly the same height as the Statue of Liberty!",
-    //     collection: "/ancient",
-    //     image: "Colossus.jpeg"
-    // },
-    // {
-    //     name: "Lighthouse of Alexandria",
-    //     location: "Egypt",
-    //     facts: ["The top of the tower had a mirror that reflected sunlight during the day.", "It was among the tallest man-made structures on Earth for many centuries.", "It was sadly destroyed by earthquakes."],
-    //     funFact: "It has also been known to be called Pharos, meaning 'lighthouse' in some languages!",
-    //     collection: "/ancient",
-    //     image: "Lighthouse.jpeg"
-    // },
-    // {
-    //     name: "Aurora Borealis",
-    //     location: "Arctic Circle",
-    //     facts: ["Also known as the Northern Lights.", "Aurora is the Roman god of the dawn; Boreas is the Greek name for the north wind.", "The Menominee Indians thought the lights were the torches of giants living in the North."],
-    //     funFact: "The Vikings thought the Northern Lights were caused by the shining weaponry of immortal warriors!",
-    //     collection: "/natural",
-    //     image: "Aurora.jpeg"
-    // },
-    // {
-    //     name: "Grand Canyon",
-    //     location: "United States of America",
-    //     facts: ["It is estimated that it began to form more than 17 million years ago.", "In some areas, the canyon is more than 1 mile deep.", "It developed when the Colorado River naturally formed its pathway through the landscape of Arizona."],
-    //     funFact: "At the widest point, the Canyon measures up to 18 miles, which would take about 6 hours to walk across!",
-    //     collection: "/natural",
-    //     image: "Canyon.jpeg"
-    // },
-    // {
-    //     name: "Great Barrier Reef",
-    //     location: "Australia ",
-    //     facts: ["It is the largest coral reef system in the world.", "It is made up of around 2900 individual reefs and 900 islands.", "Climate change is perhaps the biggest threat to the Great Barrier Reef."],
-    //     funFact: "Over 1500 different species of fish live in the Great Barrier Reef, including clownfish, star of the animated film Finding Nemo!",
-    //     collection: "/natural",
-    //     image: "Reef.jpeg"
-    // },
-    // {
-    //     name: "Harbor of Rio de Janeiro",
-    //     location: "Brazil",
-    //     facts: ["It was discovered by the Portuguese seafarer, Concalo Coelho on January 1st, 1502.", "Rio de Janeiro is Portuguese for River of January.", "The Bay spans 88 miles in length and has 50 miles of beaches."],
-    //     funFact: "The water covers a surface area of 238 square miles, which is almost enough to cover Central Park in New York 80 times over!",
-    //     collection: "/natural",
-    //     image: "RioHarbor.jpeg"
-    // },
-    // {
-    //     name: "Mount Everest",
-    //     location: "Asia",
-    //     facts: ["It is over 60 million years old.", "Everest was named 'Sagarmatha' by the Nepal Government. It means goddess of the sky.", "The summit was measured in 2020 at 29,031.69 feet."],
-    //     funFact: "It was first named Peak 15 in 1856, but later changed to 'Mount Everest' in 1865 after Sir George Everest!",
-    //     collection: "/natural",
-    //     image: "Everest.jpeg"
-    // },
-    // {
-    //     name: "Paricutin Volcano",
-    //     location: "Mexico",
-    //     facts: ["It first erupted in 1943.", "The volcano was named after the village where it is located.", "The flowing lava was fast flowing and could move at a speed of 60 meters per minute."],
-    //     funFact: "The exploding gases from the volcano reached 1060 degree celsius!",
-    //     collection: "/natural",
-    //     image: "Paricutin.jpeg"
-    // },
-    // {
-    //     name: "Victoria Falls",
-    //     location: "Africa",
-    //     facts: ["It is most well-known for being one of the widest waterfalls in the world.", "It is estimated that it started to form around 20,000 years ago.", "The widest point of the waterfall is 5,604 feet and 355 feet high."],
-    //     funFact: "It is estimated humans first started to settle near Victoria Falls during the Early Stone Age!",
-    //     collection: "/natural",
-    //     image: "VictoriaFalls.jpeg"
-    // }
+        icon: "icon_new"
+    },
+    {
+        name: "Great Pyramid of Giza",
+        location: "Egypt",
+        facts: ["It is more than five thousand years old.", "It's estimated that it took well more than two million stone blocks to make.", "It was built for a pharaoh (King) named Khufu."],
+        funFact: "When it was built it was 481 feet tall, however because of erosion, it is now 455 feet tall!",
+        collection: "/ancient",
+        image: "Pyramid.jpeg",
+        coords: {lat: 29.9792345, lng: 31.1320132},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Hanging Gardens of Babylon",
+        location: "Iraq",
+        facts: ["The gardens were thought to be about 75 feet high.", "They were built when King Nebuchadnezzar The Second was in power in Babylon.", "The plants did not actually hang. They grew from many different levels of terraces (similar to balconies)."],
+        funFact: "It would have taken 8,200 gallons of water each day to keep the plants watered. Which is roughly the same as 18,500, 2 litres bottles!",
+        collection: "/ancient",
+        image: "Gardens.jpeg",
+        coords: {lat: 32.4912413, lng: 44.435228},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Temple of Artemis",
+        location: "Turkey",
+        facts: ["The temple was burnt down by Herostratus, as he believed it would 'make him famous'.", "It was used as a house of worship to Artemis and as well as a marketplace.", "The remains of the structure were first discovered in 1869 by John Turtle Wood."],
+        funFact: "It may have been the first ever building which was constructed of Marble!",
+        collection: "/ancient",
+        image: "Artemis.jpeg",
+        coords: {lat: 37.9493601, lng: 27.3627619},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Statue of Zeus",
+        location: "Greece",
+        facts: ["It was made by the Greek sculptor Phidias around 435 BC.", "The statue was built with ivory plates and gold panels on a wooden framework.", "Zeus was the sky and thunder god in ancient Greek religion."],
+        funFact: "The statue was so tall, that if Zeus was to 'stand up', the roof would fall off the temple!",
+        collection: "/ancient",
+        image: "Zeus.jpeg",
+        coords: {lat: 37.8022947, lng: 22.1210547},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Mausoleum At Halicarnassus",
+        location: "Turkey",
+        facts: ["Construction was finished around 350 BC.", "At the very top, there was a large statue of Mausolus and Artemisia in a chariot pulled by four horses.", "It was mainly built using white marble."],
+        funFact: "Almost 19 centuries after it was completed, the Mausoleum was destroyed by earthquakes in the 1300s!",
+        collection: "/ancient",
+        image: "Mausoleum.jpeg",
+        coords: {lat: 37.0378874, lng: 27.4219277},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Colossus of Rhodes",
+        location: "Island of Rhodes",
+        facts: ["It was a symbol of strength and freedom for the people of Rhodes.", "There are rumours that a new Colossus may be built at Rhodes Harbour at some point in the future.", "Historians believe that 900 camels were used to carry away all the scrap metal."],
+        funFact: "It was 108 feet tall, which is nearly the same height as the Statue of Liberty!",
+        collection: "/ancient",
+        image: "Colossus.jpeg",
+        coords: {lat: 36.4509959, lng: 28.2236147},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Lighthouse of Alexandria",
+        location: "Egypt",
+        facts: ["The top of the tower had a mirror that reflected sunlight during the day.", "It was among the tallest man-made structures on Earth for many centuries.", "It was sadly destroyed by earthquakes."],
+        funFact: "It has also been known to be called Pharos, meaning 'lighthouse' in some languages!",
+        collection: "/ancient",
+        image: "Lighthouse.jpeg",
+        coords: {lat: 31.2124397, lng: 29.887799},
+        icon: "icon_ancient"
+    },
+    {
+        name: "Aurora Borealis",
+        location: "Arctic Circle",
+        facts: ["Also known as the Northern Lights.", "Aurora is the Roman god of the dawn; Boreas is the Greek name for the north wind.", "The Menominee Indians thought the lights were the torches of giants living in the North."],
+        funFact: "The Vikings thought the Northern Lights were caused by the shining weaponry of immortal warriors!",
+        collection: "/natural",
+        image: "Aurora.jpeg",
+        coords: {lat: 63.8793171, lng: -22.4517288},
+        icon: "icon_natural"
+    },
+    {
+        name: "Grand Canyon",
+        location: "United States of America",
+        facts: ["It is estimated that it began to form more than 17 million years ago.", "In some areas, the canyon is more than 1 mile deep.", "It developed when the Colorado River naturally formed its pathway through the landscape of Arizona."],
+        funFact: "At the widest point, the Canyon measures up to 18 miles, which would take about 6 hours to walk across!",
+        collection: "/natural",
+        image: "Canyon.jpeg",
+        coords: {lat: 36.0997623, lng: -112.1212394},
+        icon: "icon_natural"
+    },
+    {
+        name: "Great Barrier Reef",
+        location: "Australia ",
+        facts: ["It is the largest coral reef system in the world.", "It is made up of around 2900 individual reefs and 900 islands.", "Climate change is perhaps the biggest threat to the Great Barrier Reef."],
+        funFact: "Over 1500 different species of fish live in the Great Barrier Reef, including clownfish, star of the animated film Finding Nemo!",
+        collection: "/natural",
+        image: "Reef.jpeg",
+        coords: {lat: -19.3401041, lng: 145.40679},
+        icon: "icon_natural"
+    },
+    {
+        name: "Harbor of Rio de Janeiro",
+        location: "Brazil",
+        facts: ["It was discovered by the Portuguese seafarer, Concalo Coelho on January 1st, 1502.", "Rio de Janeiro is Portuguese for River of January.", "The Bay spans 88 miles in length and has 50 miles of beaches."],
+        funFact: "The water covers a surface area of 238 square miles, which is almost enough to cover Central Park in New York 80 times over!",
+        collection: "/natural",
+        image: "RioHarbor.jpeg",
+        coords: {lat: -22.8197641, lng: -43.2922626},
+        icon: "icon_natural"
+    },
+    {
+        name: "Mount Everest",
+        location: "Asia",
+        facts: ["It is over 60 million years old.", "Everest was named 'Sagarmatha' by the Nepal Government. It means goddess of the sky.", "The summit was measured in 2020 at 29,031.69 feet."],
+        funFact: "It was first named Peak 15 in 1856, but later changed to 'Mount Everest' in 1865 after Sir George Everest!",
+        collection: "/natural",
+        image: "Everest.jpeg",
+        coords: {lat: 27.9881199, lng: 86.9162203},
+        icon: "icon_natural"
+    },
+    {
+        name: "Paricutin Volcano",
+        location: "Mexico",
+        facts: ["It first erupted in 1943.", "The volcano was named after the village where it is located.", "The flowing lava was fast flowing and could move at a speed of 60 meters per minute."],
+        funFact: "The exploding gases from the volcano reached 1060 degree celsius!",
+        collection: "/natural",
+        image: "Paricutin.jpeg",
+        coords: {lat: 19.4933462, lng: -102.2601524},
+        icon: "icon_natural"
+    },
+    {
+        name: "Victoria Falls",
+        location: "Africa",
+        facts: ["It is most well-known for being one of the widest waterfalls in the world.", "It is estimated that it started to form around 20,000 years ago.", "The widest point of the waterfall is 5,604 feet and 355 feet high."],
+        funFact: "It is estimated humans first started to settle near Victoria Falls during the Early Stone Age!",
+        collection: "/natural",
+        image: "VictoriaFalls.jpeg",
+        coords: {lat: -17.9270587, lng: 25.8437687},
+        icon: "icon_natural"
+    }
 ])
