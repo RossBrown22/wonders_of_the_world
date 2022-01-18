ef5e5c3f273700d53425219fbf1b33f89a2838df
Guilherme Nunes <guilhermelsnunes@gmail.com>
2022-01-18 17:04:26 +0000

    home page updated

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,24 +3,28 @@ import MapBox from '../components/MapBox.js'
 
 const Map = ({ onWonderSelected, wonders }) => {
     return (
-        <div id="grid">
-            <header id="header">
-                <h1>The 21 Wonders of the World</h1>
-            </header>
-            <nav id="nav-bar">
-                <NavBar />
-            </nav>
-            <section id="content-1">
-                <h2>Find your Favourite Wonder!</h2>
-                <div id="mapbox-div">
-                    {wonders ? <MapBox onWonderSelected={onWonderSelected} wonders={wonders} /> : null}
-                </div>
-            </section>
-            <footer id="footer">
-                <h3>Footer</h3>
-            </footer>
+        <div className="map-page"> 
+            <div id="grid">
+                <header id="header">
+                    <h1>The 21 Wonders of the World</h1>
+                </header>
+                <nav id="nav-bar">
+                    <NavBar />
+                </nav>
+                <section id="content-1">
+                    <h2>Find your Favourite Wonder!</h2>
+                    <div id="mapbox-div">
+                        {wonders ? <MapBox onWonderSelected={onWonderSelected} wonders={wonders} /> : null}
+                    </div>
+                </section>
+                <footer id="footer">
+                    <h3>Footer</h3>
+                </footer>
+            </div>
         </div>
     );
 }
+
+
 
 export default Map;
