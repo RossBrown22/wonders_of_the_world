cef125ff6accc948bda6874ac9f08b3cc115739f
Ross Brown <rossbrown22@hotmail.co.uk>
2022-01-18 13:42:58 +0000

    confetti on quiz page

--- client/src/components/QuizForm.js
+++ client/src/components/QuizForm.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Question from './Question'
+import Confetti from 'react-confetti'
 
-const QuizForm = ({questions, onQuizSubmit}) => {
+const QuizForm = ({ questions, onQuizSubmit }) => {
 
     const [submittedAnswers, setSubmittedAnswers] = useState([])
+    const [confettiOn, setConfettiOn] = useState(false)
 
     const handleAnswerChange = (evt) => {
         const currentAnswer = evt.target.value
@@ -17,21 +19,26 @@ const QuizForm = ({questions, onQuizSubmit}) => {
         setSubmittedAnswers([])
     }
 
+    const handleConfetti = () => {
+        setConfettiOn(true)
+    }
+
     return (
         <div>
+            {confettiOn ? <Confetti width={1750} height={920} /> : null}
             <h1>This is the quiz form!</h1>
             <form onSubmit={handleQuizSubmit}>
-                
-            <div>
-                {questions.map((question, index) => {
-                    return (
-                    <div key={index}>
-                         <Question question={question} index={index} handleAnswerChange={handleAnswerChange}/>
-                    </div> 
-                    )
-                })}
-            </div>
-                <button type="submit">Submit</button>
+
+                <div>
+                    {questions.map((question, index) => {
+                        return (
+                            <div key={index}>
+                                <Question question={question} index={index} handleAnswerChange={handleAnswerChange} />
+                            </div>
+                        )
+                    })}
+                </div>
+                <button onClick={handleConfetti} type="submit">Submit</button>
             </form>
         </div>
     )
