dbcf759ae110912bb4bf831fafed87e541363a2d
Guilherme Nunes <guilhermelsnunes@gmail.com>
2022-01-19 11:45:30 +0000

    giff

--- client/src/components/Natural.js
+++ client/src/components/Natural.js
@@ -5,12 +5,15 @@ const Natural = ({wonders, selectedWonder, onWonderSelected}) => {
     return (
         <div id="grid">
             <header id="header">
-                <h1>The 7 Natural Wonders of the World</h1>
+
+            <img className="logo" src={"/img/wonderlogo.gif"}></img>
+ 
             </header>
             <nav id="nav-bar">
             <NavBar />
             </nav>
             <section id="content-1">
+            <h1>The 7 Natural Wonders of the World</h1>
               <WondersContainer wonders={wonders} selectedWonder={selectedWonder} onWonderSelected={onWonderSelected} />
             </section>
             <footer id="footer">
--- client/src/components/New.js
+++ client/src/components/New.js
@@ -6,12 +6,15 @@ function New({wonders, selectedWonder, onWonderSelected}) {
   return (
     <div id="grid">
             <header id="header">
-                <h1>The 7 New Wonders of the World</h1>
+            
+            <img className="logo" src={"/img/wonderlogo.gif"}></img>
+
             </header>
             <nav id="nav-bar">
             <NavBar />
             </nav>
             <section id="content-1">
+            <h1>The 7 New Wonders of the World</h1>
               <WondersContainer wonders={wonders} selectedWonder={selectedWonder} onWonderSelected={onWonderSelected} />
             </section>
             <footer id="footer">
